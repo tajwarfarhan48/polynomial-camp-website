ec8b10949d9115e5c74c097b33d9efcd41a44b01
Farhan Tajwar Romit <tajwarfarhan48@gmail.com>
2024-05-27 22:45:07 -0500

    Changed link behavior

--- src/widgets/Button.js
+++ src/widgets/Button.js
@@ -2,7 +2,7 @@ import '../output.css';
 
 function Button({ label, link }) {
     return (
-        <a href={link}><button className="transition duration-100 ease-in bg-teal hover:bg-darkTeal active:bg-veryDarkTeal rounded-full px-4 py-2 w-fit"><p className="font-bold">{ label }</p></button></a>
+        <a target="_blank" rel="_noreferrer" href={link}><button className="transition duration-100 ease-in bg-teal hover:bg-darkTeal active:bg-veryDarkTeal rounded-full px-4 py-2 w-fit"><p className="font-bold">{ label }</p></button></a>
     );
 }
 
